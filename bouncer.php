3a9b29aa14f927d5729cc36d6979da460aa28327
Rentuaya <cedriccana.rentuaya@bicol-u.edu.ph>
2024-09-28 20:25:08 +0800

    fixing file access

--- bouncer.php
+++ bouncer.php
@@ -10,11 +10,20 @@ if(isset($_POST['signIn'])){
    $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
    $result=$conn->query($sql);
    if($result->num_rows>0){
-    session_start();
-    $row=$result->fetch_assoc();
-    $_SESSION['email']=$row['email'];
-    header("Location: MainPage/main_page.html");
-    exit();
+      session_start();
+      $row=$result->fetch_assoc();
+      $_SESSION['email']=$row['email'];
+
+      if($row['user_type'] == 'consumer'){
+         $user_id = $row['id'];
+         header('refresh:3; MainPage/main_page.html');
+         ob_end_flush();
+      }
+      // elseif($row['user_type'] == 'admin'){
+         
+      // }
+   //  header("Location: MainPage/main_page.html");
+   //  exit();
    }
    else{
     echo "Not Found, Incorrect Email or Password";
